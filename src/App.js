57cc80be1416ddea1cc27b2044a14fad9edc1bb2
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-19 12:50:28 -0300

    Proj. Finalizado

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Intervalo from './components/Intervalo';
-import Media from './components/Media';
-import Soma from './components/Soma';
-import Sorteio from './components/Sorteio';
+import Intervalo from "./components/Intervalo";
+import Media from "./components/Media";
+import Soma from "./components/Soma";
+import Sorteio from "./components/Sorteio";
 
 function App() {
-
   return (
     <div className="App">
       <h1>Exercício React-Redux</h1>
-      <div className='linha'>
-        <Intervalo 
-        />
+      <div className="linha">
+        <Intervalo />
       </div>
-      <div className='linha' >
-        <Media/>
+      <div className="linha">
+        <Media />
         <Soma />
         <Sorteio />
       </div>
