e0b4b4cec3693e5d91e67191387da617ed423f30
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-19 11:05:51 -0300

    Intervalo

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Card from './components/Card';
+import Intervalo from './components/Intervalo';
 
 function App() {
   return (
     <div className="App">
       <h1>Exercício React-Redux</h1>
       <div className='linha'>
-        <Card title='Card #01' red>
-          X
-        </Card>
+        <Intervalo />
+      </div>
+      <div className='linha' >
         <Card title='Card #02' green>
           Y
         </Card>
-      </div>
-      <div className='linha' >
         <Card title='Card #03' blue>
           Z
         </Card>
---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,21 @@
+import './Intervalo.css'
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+    return (
+        <Card title="Intervalode Números" red>
+            <div className='Intervalo'>
+                <span>
+                    <strong>Mínimo: </strong>
+                    <input type="number" value={0} readOnly/>
+                </span>
+                <span>
+                    <strong>Maximo: </strong>
+                    <input type="number" value={10} readOnly/>
+                </span>
+            </div>
+
+        </Card>
+    )
+}
