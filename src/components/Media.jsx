2206a81f980ba2c3f0bcbb3baac07af84cb0fa7e
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-19 11:30:32 -0300

    Sem redux

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Card from './Card'
 
 export default props => {
+    const {min, max} = props
     return (
         <Card title="Media" green>
             <div>
                 <span>
                     <span>Resultado: </span>
-                    <strong>5</strong>
-
+                    <strong>{(max+min) / 2}</strong>
                 </span>
             </div>
-
         </Card>
     )
 }
